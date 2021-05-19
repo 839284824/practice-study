5f444e979f2bcfc784a61e2ec76a67d06407f8a1
gongzhao <gongzhao.gz@alibaba-inc.com>
2021-05-19 17:11:27 +0800

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 public class Main {
 
-    public static void main(String[] a) throws Exception {
+    public static void main(String[] args) throws Exception {
+
+        String json = "{'did':'+idOF2KNky4CAWoLKdTv1TgV'}";
+
+
+        JSONObject jsonObject = JSON.parseObject(URLDecoder.decode(URLEncoder.encode(json)));
+
+        System.out.println("jsonObject:" + jsonObject.get("did"));
+
 
     }
-
-
 }
