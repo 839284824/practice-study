9d7c96d85e11880ffc0f88f6aa477acf4f14c129
gongzhao.gz <gongzhao.gz@alibaba-inc.com>
2022-03-18 18:06:08 +0800

    thread local

---
+++ src/main/java/practice/thread/TransmitThreadUtil.java
@@ -0,0 +1,46 @@
+package practice.thread;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 异步线程池之间相互传递变量
+ */
+@Slf4j
+public final class TransmitThreadUtil {
+
+    private static TransmittableThreadLocal<Map<String, String>> threadLocal = new TransmittableThreadLocal(true){
+        @Override
+        public Map<String, Object> initialValue() {
+            return new HashMap<>(16);
+        }
+    };
+
+
+    public static void put(String key, String value) {
+        try {
+            threadLocal.get().put(key, value);
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static String get(String key) {
+        try {
+            return threadLocal.get().get(key);
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        TransmitThreadUtil.put("aaid","test");
+        TransmitThreadUtil.put("traceId","adsad");
+        log.info(TransmitThreadUtil.get("aaid"));
+        log.info(TransmitThreadUtil.get("traceId"));
+    }
+
+}
